7d13cd490643eb2c3997fb2e5f0d91272bbb98e7
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-19 20:08:56 +0100

    finished

--- app/src/main/java/example/codeclan/com/magazines/TopMagazines.java
+++ app/src/main/java/example/codeclan/com/magazines/TopMagazines.java
@@ -13,8 +13,15 @@ public class TopMagazines {
     public TopMagazines() {
         list = new ArrayList<Magazine>();
         list.add(new Magazine(1, "Ambrosia", 2016));
-        list.add(new Magazine(2, "Cherry Bomb", 2017));
+        list.add(new Magazine(2, "Cherry Bombe", 2017));
         list.add(new Magazine(3, "Junko", 2015));
+        list.add(new Magazine(4, "Drift", 2013));
+        list.add(new Magazine(5, "Beirut Cooks", 2015));
+        list.add(new Magazine(6, "Standart", 2010));
+        list.add(new Magazine(7, "Elementum", 2017));
+        list.add(new Magazine(8, "In Clover", 2015));
+        list.add(new Magazine(9, "Lunch Lady", 2016));
+        list.add(new Magazine(10, "Tank", 2017));
     }
 
     public ArrayList<Magazine> getList(){
