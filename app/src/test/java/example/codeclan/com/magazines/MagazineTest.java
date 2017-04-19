a03bd7f33eb7e748c62dbc142dfb73b8818d7244
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-19 15:57:43 +0100

    test file added and test for getName added

--- app/src/test/java/example/codeclan/com/magazines/MagazineTest.java
+++ app/src/test/java/example/codeclan/com/magazines/MagazineTest.java
@@ -1,8 +1,25 @@
 package example.codeclan.com.magazines;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 19/04/2017.
  */
 
 public class MagazineTest {
+
+    Magazine magazine;
+
+    @Before
+    public void before(){
+        Magazine magazine = new Magazine(1, "Ambrosia", 2016);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Ambrosia", magazine.getName());
+    }
 }
