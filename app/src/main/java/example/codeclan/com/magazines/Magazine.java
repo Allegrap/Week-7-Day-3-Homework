779bc82b1ecf8c2c84b112d7065d6c3d654fcb00
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-19 15:52:51 +0100

    setup of magazine class

---
+++ app/src/main/java/example/codeclan/com/magazines/Magazine.java
@@ -0,0 +1,16 @@
+package example.codeclan.com.magazines;
+
+/**
+ * Created by user on 19/04/2017.
+ */
+
+public class Magazine {
+
+    private int ranking;
+    private String name;
+    private int year;
+
+    public Magazine(int ranking, String name, int year){
+        
+    }
+}
